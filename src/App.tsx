927c7589abf5eb0734a7d4496de61e22320af072
Teemu Näsänen <55226758+CyperNastee@users.noreply.github.com>
2021-02-02 13:07:31 +0200

    master

--- src/App.tsx
+++ src/App.tsx
@@ -54,15 +54,15 @@ const App: React.FC = () => (
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
             <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+            <IonLabel>Home</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
             <IonIcon icon={ellipse} />
-            <IonLabel>Tab 2</IonLabel>
+            <IonLabel>Second</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
             <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
+            <IonLabel>Third</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
